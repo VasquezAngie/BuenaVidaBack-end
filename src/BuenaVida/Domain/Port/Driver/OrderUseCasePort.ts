eb808fe3e1f5241e84f9c2dbf581b90e4e26bbf9
valentinaxd <vale.tellez78@gmail.com>
2025-03-07 14:16:57 -0500

    Update OrderUseCasePort.ts
    .

--- src/BuenaVida/Domain/Port/Driver/OrderUseCasePort.ts
+++ src/BuenaVida/Domain/Port/Driver/OrderUseCasePort.ts
@@ -3,20 +3,18 @@ import Order from '../../Order/Order';
 import { ProductInterface } from '../../Product/AbstractProduct';
 
 export default interface OrderUseCasePort {
-    // listOrders(): Promise<Order[]>;
-    // getOrderById(orderId: number): Promise<Order | null>;
-    // createOrder(order: Order): Promise<void>;
-    getAllOrders(): Promise<Order[]>;
-    createOrder(order: Order): Promise<void>;
-    guardar(order: Order): Promise<void>;
-    actualizar(order: Order): Promise<void>;
-    listarPorUsuario(userId: number): Promise<Order[]>;
+    
+  crearOrden(order: OrderInterface): Promise<void>
+  calcularTotal(idPedido: number): Promise<number>
+  getEstadoOrden(idPedido: number): Promise<string>
+  actualizarEstadoOrden(idPedido:number, nuevoEstado: string): Promise<void>
 
-    newOrder(): Promise<Order>;
-    obtenerPorId(orderId: number): Promise<Order>;
-    actualizarEstado(order: Order): Promise<void>;
-    actualizarEstado(order: Order): Promise<void>;
-    actualizarEstado(order: Order): Promise<void>;
+  
+  traerPedido(id: number): Promise<Order | null>
+  todosPedidos(): Promise<Order[]>
+  actualizarDireccion(id: number, newAddress: string): Promise<void>
+  actualizarCelular(id: number, newPhone: number): Promise<void>
+  eliminarPedido(pedidoId: number): Promise<void> 
+  pedidoUser(userId: number): Promise<Order[] | null> 
 
-
-  }
+}
